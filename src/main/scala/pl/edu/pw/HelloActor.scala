19a8ba2e0f99014dc928e86f385126862b0f112c
AMichalak10 <arkadiusz.michalak10@external.t-mobile.pl>
2020-06-02 01:17:55 +0200

    Added helloWord actor example

---
+++ src/main/scala/pl/edu/pw/HelloActor.scala
@@ -0,0 +1,11 @@
+package pl.edu.pw
+
+import akka.actor.Actor
+
+
+class HelloActor extends Actor {
+  def receive = {
+    case "hello" => println("hello back at you")
+    case _       => println("huh?")
+  }
+}
---
+++ src/main/scala/pl/edu/pw/LinearEquationMain.scala
@@ -0,0 +1,11 @@
+package pl.edu.pw
+
+import akka.actor.{ActorSystem, Props}
+
+object LinearEquationMain extends App {
+  val system = ActorSystem("HelloSystem")
+  // default Actor constructor
+  val helloActor = system.actorOf(Props[HelloActor], name = "helloactor")
+  helloActor ! "hello"
+  helloActor ! "buenos dias"
+}
